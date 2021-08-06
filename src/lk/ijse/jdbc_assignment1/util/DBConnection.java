08dfedd246d230f3952d3d55ba2d53997c20c51e
Manoj Randeni <randeni.ramr@gmail.com>
2021-08-06 14:49:42 +0530

    Change password for dbms

--- src/lk/ijse/jdbc_assignment1/util/DBConnection.java
+++ src/lk/ijse/jdbc_assignment1/util/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
     private DBConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7","root","mysql");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7","root","7251mMm7251");
 
             Runtime.getRuntime().addShutdownHook(new Thread(()->{
 
